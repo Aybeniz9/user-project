b738cba436464c60c9329c671e54f4ead345e667
aybeniz <aybenizkazimova15@gmail.com>
2024-07-29 16:41:29 +0400

    user project with jdbc.

--- src/main/java/az/edu/turing/userproject/UserProjectApplication.java
+++ src/main/java/az/edu/turing/userproject/UserProjectApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UserProjectApplication.class, args);
     }
